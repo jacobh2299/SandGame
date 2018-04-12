2568995c82815f187a941b2232a64c23623d0255
jacobh2299 <31513783+jacobh2299@users.noreply.github.com>
2018-04-12 09:53:42 -0600

    MADE A WATER SAND AND METAL GAME !!!!!!

--- src/SandLab.java
+++ src/SandLab.java
@@ -7,6 +7,7 @@ public class SandLab
   //add constants for particle types here
   public static final int EMPTY = 0;
   public static final int METAL = 1;
+  public static final int SAND = 2;
   
   //do not add any more fields below
   private int[][] grid;
@@ -23,13 +24,15 @@ public class SandLab
     String[] names;
     // Change this value to add more buttons
     //Step 4,6
-    names = new String[2];
+    names = new String[4];
     // Each value needs a name for the button
     names[EMPTY] = "Empty";
     names[METAL] = "Metal";
+    names[SAND] = "Sand";
+    names[WATER] = "Water";
     
     //1. Add code to initialize the data member grid with same dimensions
-    
+    grid = new int [numRows][numCols];
     
     display = new SandDisplay("Falling Sand", numRows, numCols, names);
   }
@@ -38,7 +41,7 @@ public class SandLab
   private void locationClicked(int row, int col, int tool)
   {
     //2. Assign the values associated with the parameters to the grid
-   
+   grid[row][col] = tool;
   }
 
   //copies each element of grid into the display
@@ -46,6 +49,32 @@ public class SandLab
   {
       //Step 3
    //Hint - use a nested for loop
+	  for(int i = 0; i < grid.length; i++)
+	  {
+		  for(int j = 0; j < grid[0].length ; j++)
+		  {
+			  Color gray = Color.GRAY;
+			  Color black = Color.BLACK;
+			  Color yellow = new Color(220, 160, 90);
+			  Color blue = Color.BLUE;
+			  if(grid[i][j] == EMPTY)
+			  {
+				  display.setColor(i, j, black);
+			  }
+			  else if(grid[i][j] == METAL)
+			  {
+				  display.setColor(i, j, gray);
+			  }
+			  else if(grid[i][j] == SAND)
+			  {
+				  display.setColor(i, j, yellow);
+			  }
+			  else
+			  {
+				  display.setColor(i, j, blue);
+			  }
+		  }
+	  }
     
   }
 
@@ -59,6 +88,47 @@ public class SandLab
     //int someRandom = (int) (Math.random() * scalar)
     //remember that you need to watch for the edges of the array
     
+	  Random rand = new Random();
+	  int direction = rand.nextInt(2);
+	  int direction2 = rand.nextInt(2);
+	  
+	  
+	  //Sand Random
+	  int randomRow = (int) (Math.random() * grid.length - 1);
+	  int randomCol = (int) (Math.random() * grid[0].length -1);
+	  //Sand Physics
+	  if(grid[randomRow][randomCol] == SAND && grid[randomRow + 1][randomCol] == EMPTY)
+	  {
+		  grid[randomRow][randomCol] = EMPTY;
+		  grid[randomRow + 1][randomCol] = SAND;
+	  }
+	  if(grid[randomRow][randomCol] == SAND && grid[randomRow + 1][randomCol] == WATER)
+	  {
+		  grid[randomRow][randomCol] = WATER;
+		  grid[randomRow + 1][randomCol] = SAND;
+	  }
+	  //Water Physics
+	  if(grid[randomRow][randomCol] == WATER && grid[randomRow + 1][randomCol] == EMPTY)
+	  {
+
+		  grid[randomRow][randomCol] = EMPTY;
+		  grid[randomRow + 1][randomCol] = WATER;
+		  
+		  if(randomCol -1 != -1 && direction == 0 && grid[randomRow + 1][randomCol - 1] == EMPTY)
+		  {
+			  grid[randomRow + 1][randomCol - 1] = WATER;
+			  grid[randomRow + 1][randomCol] = EMPTY;
+		  }
+		  else if(direction == 1 && grid[randomRow + 1][randomCol + 1] == EMPTY)
+		  {
+			  grid[randomRow + 1][randomCol + 1] = WATER;
+			  grid[randomRow + 1][randomCol] = EMPTY;
+		  }
+		  else
+		  {
+			  
+		  }
+	  }
     
   }
   
